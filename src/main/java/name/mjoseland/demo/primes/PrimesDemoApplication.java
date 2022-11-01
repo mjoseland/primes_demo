ca4c8bec4c34d16a79707d6a5b8f656568a14073
Mal Joseland <software@mjoseland.name>
2022-11-01 16:14:37 +1000

    renamed project (DemoVirtualThreads -> PrimesDemo)

--- src/main/java/name/mjoseland/demo/primes/PrimesDemoApplication.java
+++ src/main/java/name/mjoseland/demo/primes/PrimesDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoVirtualThreadsApplication {
+public class PrimesDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoVirtualThreadsApplication.class, args);
+        SpringApplication.run(PrimesDemoApplication.class, args);
     }
 
 }
